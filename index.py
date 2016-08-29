ce1b6ab2c2c01df78ac0f9f3f4f317f6d660e6ae
DUSHYANT SINGH <dushyant7917@gmail.com>
2016-08-29 18:51:56 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,31 @@
+import pprint
+
+import pymongo
+from pymongo import MongoClient
+
+from pyelasticsearch import ElasticSearch
+es = ElasticSearch('http://localhost:9200/')
+
+client = MongoClient('localhost:27017')
+db = client.Hindi_News
+
+l = db.News.find()
+
+ll = []
+
+for item in l:
+    ll.append({'url': item['url'],
+               'data': {'title': item['title'],
+                        'summary': item['summary'],
+                        'description': item['description'],
+                        'keywords': item['keywords']
+                       },
+               'date_time': item['date_time'], 
+               'image_url': item['image_url']
+              })
+
+es.bulk((es.index_op(doc) for doc in ll), index='test-index3', doc_type='doc')
+       
+
+
+
