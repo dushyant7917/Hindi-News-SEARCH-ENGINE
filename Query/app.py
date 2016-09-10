b8f813d8756020666d3abb7a83517c945fc61eb8
DUSHYANT SINGH <dushyant7917@gmail.com>
2016-09-10 17:28:41 +0530

    Update app.py

--- Query/app.py
+++ Query/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template("page.html")
+    return render_template("search.html")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
