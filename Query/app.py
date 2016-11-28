cb2950eadce795931cae9729e05733f4f71836b9
DUSHYANT SINGH <dushyant7917@gmail.com>
2016-11-28 20:05:40 +0530

    Update app.py

--- Query/app.py
+++ Query/app.py
@@ -3,26 +3,28 @@ from flask import request
 from flask import render_template, redirect, url_for
 from elasticsearch import Elasticsearch
 
+
 app = Flask(__name__)
 
 
-@app.route('/',methods=['GET'])
-def my_form():
-    return render_template("search.html")
+@app.route('/',methods = ['GET','POST'])
+def index():
+    if request.method == 'GET':
+        return render_template("search.html")
+    else:
+        query = request.form['text']
+        return redirect(url_for('search', query = query))
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    query = request.form['text']
-    return redirect(url_for("api_call", text=query))
-
-
-@app.route('/api/v1.0/search/<string:text>',methods=['GET'])
-def api_call(text):
-    es = Elasticsearch()
-    results = es.search(index="test-index3", doc_type="doc", body={"size":18,"query":{"query_string":{"query":text}}})
-    return render_template("results.html", res=results)
+@app.route('/search/?=<query>', methods = ['GET', 'POST'])
+def search(query):
+    if request.method == 'GET':
+        es = Elasticsearch()
+        results = es.search(index="test-index3", doc_type="doc", body={"size":18,"query":{"query_string":{"query":query}}})
+        return render_template('results.html', results = results, search_text = query)
+    else:
+        return "Some Error Occured!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int('5000'), debug='True')
+    app.run( host='0.0.0.0', port=int('5000'), debug = True )
