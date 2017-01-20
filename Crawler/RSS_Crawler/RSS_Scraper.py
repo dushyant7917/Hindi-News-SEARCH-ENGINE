a66aa1319dd01cedd7c7bd3a1bbad11a33b94609
DUSHYANT SINGH <dushyant7917@gmail.com>
2017-01-20 14:23:25 +0530

    Update RSS_Scraper.py

--- Crawler/RSS_Crawler/RSS_Scraper.py
+++ Crawler/RSS_Crawler/RSS_Scraper.py
@@ -22,49 +22,54 @@ NEWS_URLS = []
 
 # Continuous scraping of web after gap of 2 minutes
 while True:
-    
-    req = requests.get(RSS_urls[0], headers=agent)
-    soup = BeautifulSoup(req.content, "xml")
-    for i in soup.find_all('link'):
-        if i.text != "http://www.amarujala.com" and i.text not in NEWS_URLS:
-            NEWS_URLS.append(i.text)
+
+    for article_link in RSS_urls:
+        req = requests.get(article_link, headers=agent)
+        soup = BeautifulSoup(req.content, "xml")
+        for i in soup.find_all('link'):
+            if i.text != "http://www.amarujala.com" and i.text not in NEWS_URLS:
+                NEWS_URLS.append(i.text)
 
 
     for link in NEWS_URLS:
-        req = requests.get(link, headers=agent)
-        soup = BeautifulSoup(req.content, "html.parser")
+        try:
+            req = requests.get(link, headers=agent)
+            soup = BeautifulSoup(req.content, "html.parser")
 
-        # Finding news title
-        title = soup.find('meta', attrs={'name': 'twitter:title'})
-        news_title = title.get('content').replace("- Amarujala", "")
-        #print(news_title)
+            # Finding news title
+            title = soup.find('meta', attrs={'name': 'twitter:title'})
+            news_title = title.get('content').replace("- Amarujala", "")
+            #print(news_title)
 
-        # Finding news image
-        img = soup.find('meta', attrs={'name': 'twitter:image'})
-        news_image = img.get('content')
-        #print(news_image)
+            # Finding news image
+            img = soup.find('meta', attrs={'name': 'twitter:image'})
+            news_image = img.get('content')
+            #print(news_image)
 
-        # Finding news image
-        kw = soup.find('meta', attrs={'name': 'keywords'})
-        news_keywords = kw.get('content').split(",")
-        #print(news_keywords)
+            # Finding news image
+            kw = soup.find('meta', attrs={'name': 'keywords'})
+            news_keywords = kw.get('content').split(",")
+            #print(news_keywords)
 
-        # Finding news summary
-        summary = soup.find('meta', attrs={'property': 'og:description'})
-        news_summary = summary.get('content')
-        #print(news_summary)
+            # Finding news summary
+            summary = soup.find('meta', attrs={'property': 'og:description'})
+            news_summary = summary.get('content')
+            #print(news_summary)
 
-        # Finding news date
-        dt = soup.find('span', attrs={'datetime': True})
-        news_date_time = dt.text.split(", ")[-1]
-        #print(news_date_time)
+            # Finding news date
+            dt = soup.find('span', attrs={'datetime': True})
+            news_date_time = dt.text.split(", ")[-1]
+            #print(news_date_time)
 
-        # Inserting data in database
-        result = collection.find_one({'url': link})
-        if result == None:
-            collection.insert_one({'url': link, 'title': news_title, 'summary': news_summary, 'date_time': news_date_time, 'image': news_image, 'keywords': news_keywords})
-        else:
-            print("URL -> " + link + " has already been scraped!\nMoving to next link in the queue...")
+            # Inserting data in database
+            result = collection.find_one({'url': link})
+            if result == None:
+                collection.insert_one({'url': link, 'title': news_title, 'summary': news_summary, 'date_time': news_date_time, 'image': news_image, 'keywords': news_keywords})
+            else:
+                print("URL -> " + link + " has already been scraped!\nMoving to next link in the queue...")
+
+        except:
+            print("Error occured!")
 
     # Another scraping attempt for fresh news will be made after 5 seconds
     time.sleep(120)
