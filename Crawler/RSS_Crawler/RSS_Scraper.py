2a7506e338336dc831055a7f08cd80ffb84ca994
DUSHYANT SINGH <dushyant7917@gmail.com>
2017-01-21 11:48:44 +0530

    Update RSS_Scraper.py

--- Crawler/RSS_Crawler/RSS_Scraper.py
+++ Crawler/RSS_Crawler/RSS_Scraper.py
@@ -6,6 +6,18 @@ import RSS_Links
 
 import time
 
+def RSSlinks():
+    agent = {'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'}
+
+    RSS_URLS = []
+    req = requests.get("http://www.amarujala.com/rss", headers=agent)
+    soup = BeautifulSoup(req.content, "html.parser")
+    for i in soup.find_all('td', attrs={"style": "width: 400px;"}):
+        if i.text[0] == 'h':
+            RSS_URLS.append(i.text)
+
+    return RSS_URLS
+
 # Using Mongodb as database
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
@@ -13,7 +25,7 @@ db = client['RSS-database']
 collection = db['RSS-news']
 
 # Getting all the RSS links
-RSS_urls = RSS_Links.RSSlinks()
+RSS_urls = RSSlinks()
 
 agent = {'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'}
 
