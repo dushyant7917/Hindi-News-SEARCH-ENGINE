84245dd2376a4c6d336cfedc7a78070b413b7922
DUSHYANT SINGH <dushyant7917@gmail.com>
2017-02-28 21:34:19 +0530

    Script to index data on AWS ES cluster

---
+++ Index/AWS_index.py
@@ -0,0 +1,45 @@
+import os, base64, re, logging
+from elasticsearch import Elasticsearch
+
+# Log transport details (optional):
+logging.basicConfig(level=logging.INFO)
+
+print("\n\nIndexing data...\n\n")
+
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost:27017')
+db = client.Hindi_News
+
+l = db.News.find()
+
+total = db.News.count()
+count = 0
+prv = -1
+
+while count<total:
+    try:
+        # Instantiate the new Elasticsearch connection
+        es = Elasticsearch("AWS Elastic Search Cluster API End-Point")
+        count = 0
+        for item in xrange(0,total):
+            if count > prv:
+                prv = count
+
+                es.index('hn', 'doc', {'url': l[item]['url'],
+                                              'title': l[item]['title'],
+                                              'summary': l[item]['summary'],
+                                              'description': l[item]['description'],
+                                              'keywords': l[item]['keywords'],
+                                              'date_time': l[item]['date_time'],
+                                              'image_url': l[item]['image_url']
+                                             })
+
+                count = count + 1
+                print count, item+1
+            else:
+                count = count + 1
+
+    except Exception as e:
+        print(str(e))
